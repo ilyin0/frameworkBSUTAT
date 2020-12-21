909cfcd47936f43773311b0010a66f74850a51d0
ilyin0 <ilyindeveloper@gmail.com>
2020-12-22 01:24:39 +0300

    Add Address model class

---
+++ src/test/java/model/Address.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.Objects;
+
+public class Address {
+
+    private String city;
+    private String street;
+    private int houseNumber;
+
+    public Address(String city, String street, int houseNumber) {
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber=" + houseNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return houseNumber == address.houseNumber &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, houseNumber);
+    }
+}
