938f1880a76055bb83514eff763e029887c543d1
ilyin0 <ilyindeveloper@gmail.com>
2020-12-22 13:10:58 +0300

    Add TestListener

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import util.TestListener;
 
+@Listeners({TestListener.class})
 public class CommonConditions {
     protected WebDriver driver;
 
