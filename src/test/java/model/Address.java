a47d11857151d938014c36b9ce926768881d1848
ilyin0 <ilyindeveloper@gmail.com>
2020-12-26 18:25:48 +0300

    Add new fields to Address. Refactor AddressCreator and dev.properties accordingly it.

--- src/test/java/model/Address.java
+++ src/test/java/model/Address.java
@@ -7,11 +7,17 @@ public class Address {
     private String city;
     private String street;
     private String houseNumber;
+    private String apartmentNumber;
+    private String floor;
+    private String entrance;
 
-    public Address(String city, String street, String houseNumber) {
+    public Address(String city, String street, String houseNumber, String apartmentNumber, String floor, String entrance) {
         this.city = city;
         this.street = street;
         this.houseNumber = houseNumber;
+        this.apartmentNumber = apartmentNumber;
+        this.floor = floor;
+        this.entrance = entrance;
     }
 
     public String getCity() {
@@ -38,13 +44,28 @@ public class Address {
         this.houseNumber = houseNumber;
     }
 
-    @Override
-    public String toString() {
-        return "Address{" +
-                "city='" + city + '\'' +
-                ", street='" + street + '\'' +
-                ", houseNumber=" + houseNumber +
-                '}';
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(String entrance) {
+        this.entrance = entrance;
     }
 
     @Override
@@ -52,13 +73,41 @@ public class Address {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Address address = (Address) o;
-        return Objects.equals(houseNumber, address.houseNumber) &&
-                Objects.equals(city, address.city) &&
-                Objects.equals(street, address.street);
+        return Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(houseNumber, address.houseNumber) &&
+                Objects.equals(apartmentNumber, address.apartmentNumber) &&
+                Objects.equals(floor, address.floor) &&
+                Objects.equals(entrance, address.entrance);
     }
+
+    public boolean equalsIgnoreCase(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return city.equalsIgnoreCase(address.city) &&
+                street.equalsIgnoreCase(address.city) &&
+                houseNumber.equalsIgnoreCase(address.houseNumber) &&
+                apartmentNumber.equalsIgnoreCase(address.apartmentNumber) &&
+                floor.equalsIgnoreCase(address.floor) &&
+                entrance.equalsIgnoreCase(address.entrance);
+    }
+
 
     @Override
     public int hashCode() {
-        return Objects.hash(city, street, houseNumber);
+        return Objects.hash(city, street, houseNumber, apartmentNumber, floor, entrance);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber='" + houseNumber + '\'' +
+                ", apartmentNumber='" + apartmentNumber + '\'' +
+                ", floor='" + floor + '\'' +
+                ", entrance='" + entrance + '\'' +
+                '}';
     }
 }
--- src/test/java/service/AddressCreator.java
+++ src/test/java/service/AddressCreator.java
@@ -7,10 +7,17 @@ public class AddressCreator {
     public static final String TESTDATA_CITY = "testdata.address.city";
     public static final String TESTDATA_STREET = "testdata.address.street";
     public static final String TESTDATA_HOUSE_NUMBER = "testdata.address.housenumber";
+    public static final String TESTDATA_APARTMENT_NUMBER = "testdata.address.apartmentnumber";
+    public static final String TESTDATA_FLOOR = "testdata.address.floor";
+    public static final String TESTDATA_ENTRANCE = "testdata.address.entrance";
 
     public static Address withCredentialsFromProperties() {
         return new Address(TestDataReader.getTestData(TESTDATA_CITY),
-                TestDataReader.getTestData(TESTDATA_STREET), TestDataReader.getTestData(TESTDATA_HOUSE_NUMBER));
+                TestDataReader.getTestData(TESTDATA_STREET),
+                TestDataReader.getTestData(TESTDATA_HOUSE_NUMBER),
+                TestDataReader.getTestData(TESTDATA_APARTMENT_NUMBER),
+                TestDataReader.getTestData(TESTDATA_FLOOR),
+                TestDataReader.getTestData(TESTDATA_ENTRANCE));
     }
 
 }
