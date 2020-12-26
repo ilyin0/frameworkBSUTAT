5b51e12c5c5834a6a27ba86f4f09576bc510fdd3
ilyin0 <ilyindeveloper@gmail.com>
2020-12-26 19:12:09 +0300

    Add ProfileAddressTest.testRemoveAddress

--- src/test/java/test/ProfileAddressTest.java
+++ src/test/java/test/ProfileAddressTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.MainPage;
 import pages.ProfilePage;
@@ -27,18 +28,19 @@ public class ProfileAddressTest extends CommonConditions {
 
     @Test
     public void testAddAddress() {
-        MainPage page = new MainPage(driver);
-        page.openPage().closeAds();
-
         Address address = AddressCreator.withCredentialsFromProperties();
-
-        ProfilePage profilePage = page.login(UserCreator.withCredentialsFromProperty()).
-                clickAndGoToProfile();
-
         profilePage.addAddress(address);
-
         Assert.assertTrue(address.equalsIgnoreCase(profilePage.getLastAddedAddress()), "Last added address is not right");
     }
 
+    @Test
+    public void testRemoveAddress() {
+        Address address = AddressCreator.withCredentialsFromProperties();
+        profilePage.addAddress(address);
+        int beginAddressesCount = profilePage.getAddressesCount();
+        profilePage.removeLastAddress();
+        int endAddressesCount = profilePage.getAddressesCount();
+        Assert.assertEquals(endAddressesCount, beginAddressesCount - 1, "Addresses list didn't get reduced");
+    }
 
 }
