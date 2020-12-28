4ddcb7228ce683d71d7094cca872eaff0b9b0998
ilyin0 <ilyindeveloper@gmail.com>
2020-12-29 01:03:41 +0300

    Add pizza adding, removing methods and additional methods for it

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,14 +1,19 @@
 package pages;
 
+import model.Pizza;
 import model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class MainPage extends AbstractPage {
 
@@ -58,5 +63,93 @@ public class MainPage extends AbstractPage {
         driver.get(MAIN_PAGE_URL);
         waitJavascriptIsExecuted(WAIT_TIMEOUT_SECONDS);
         return this;
+    }
+
+    public WebElement getPizzaProductCard(Pizza pizza) {
+        return getPizzaProductCard(pizza.getName());
+    }
+
+    public WebElement getPizzaProductCard(String pizzaName) {
+        return waitAndGet(By.xpath("//div[@class='product-card__title'][text()='" + pizzaName + "']/parent::div/parent::div"));
+    }
+
+    public MainPage addPizzaToCard(Pizza pizza) {
+        WebElement pizzaCard = getPizzaProductCard(pizza);
+        List<WebElement> selectList = pizzaCard.findElements(By.tagName("select"));
+
+        Select selectSize = new Select(selectList.get(0));
+        selectSize.selectByValue(pizza.getSize());
+        Select selectSidetype = new Select(selectList.get(1));
+        selectSidetype.selectByValue(pizza.getSideType());
+
+        pizzaCard.findElement(By.className("product-card__actions")).click();
+
+        return this;
+    }
+
+    public MainPage openCart() {
+        if (!isCartOpened()) divCartButton.click();
+        return this;
+    }
+
+    public boolean isCartOpened() {
+        try {
+            driver.findElement(By.className("cart-button__mini-bag"));
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public WebElement getLastAddedToCartPizzaCard() {
+        return getInCartPizzasCards().get(0);
+    }
+
+    public List<WebElement> getInCartPizzasCards() {
+        openCart();
+        return driver.findElements(By.className("product-card--mini-bag"));
+    }
+
+    public Pizza getPizzaFromMiniBagCard(WebElement pizzaCard) {
+        String name = pizzaCard.findElement(By.className("product-card__title")).getText();
+        String description = pizzaCard.findElement(By.className("product-card__description")).getText();
+        String[] sidetypeAndSize = description.split(", ");
+        String sidetype = sidetypeAndSize[0];
+        String size = sidetypeAndSize[1];
+
+        return new Pizza(name, size, sidetype);
+    }
+
+    public Pizza getLastAddedToCartPizza() {
+        return getPizzaFromMiniBagCard(getLastAddedToCartPizzaCard());
+    }
+
+    public int pizzasInCartCount() {
+        return getInCartPizzasCards().size();
+    }
+
+    public WebElement getPizzaMiniBagCard(Pizza pizza) {
+        return getPizzaMiniBagCard(pizza.getName());
+    }
+
+    public String getPizzaNameFromMiniBagCard(WebElement pizzaCard) {
+        return pizzaCard.findElement(By.className("product-card__title")).getText();
+    }
+
+    public WebElement getPizzaMiniBagCard(String name) {
+        List<WebElement> pizzasList = getInCartPizzasCards();
+        for (WebElement pizzaCard : pizzasList)
+            if (getPizzaNameFromMiniBagCard(pizzaCard).equalsIgnoreCase(name)) return pizzaCard;
+        return null;
+    }
+
+    public MainPage removePizzaFromCart(String name) {
+        WebElement pizzaCard = getPizzaMiniBagCard(name);
+        pizzaCard.findElement(By.className("product-card__button-close")).click();
+        return this;
+    }
+
+    public MainPage removePizzaFromCart(Pizza pizza) {
+        return removePizzaFromCart(pizza.getName());
     }
 }
