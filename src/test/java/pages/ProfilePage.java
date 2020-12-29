b36ebc8c18fafdbd5bc5fa9b1c4448712195a865
ilyin0 <ilyindeveloper@gmail.com>
2020-12-29 10:45:37 +0300

    Refactor ProfilePage using By locators fields

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,9 +22,6 @@ public class ProfilePage extends AbstractPage {
 
     @FindBy(xpath = "//a[@class='profile__title-link active']")
     private WebElement aLogout;
-
-    @FindBy(xpath = "//button[text()='\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0430\u0434\u0440\u0435\u0441']")
-    private WebElement btnAddAddress;
 
     @FindBy(tagName = "select")
     private WebElement sltCity;
@@ -48,11 +44,17 @@ public class ProfilePage extends AbstractPage {
     @FindBy(xpath = "//button[@type='submit'][text()='\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c']")
     private WebElement btnSubmitAddress;
 
-    @FindBy(className = "notification")
-    private WebElement notification;
+    private final String choiceButtonStringLocator = "//button[./div/text()='%s' and ./div/text()='%s']";
 
-    @FindBy(xpath = "//div[@class='user-streets']//li")
-    private List<WebElement> liAddressesDivs;
+    private final By notificationLocator = By.className("notification");
+    private final By btnCloseNotificationInNotificationLocator = By.className("notification__close-button");
+    private final By btnAddAddressLocator = By.xpath("//button[text()='\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0430\u0434\u0440\u0435\u0441']");
+    private final By divChooseStreetLocator = By.xpath("//div[div/text()='\u0423\u043b\u0438\u0446\u0430']");
+    private final By streetSearchInputLocator = By.xpath("//div[@class='search-street__modal-content']//input");
+    private final By notificationContentInNotificationLocator = By.className("notification__content");
+    private final By liAddressesDivsLocator = By.xpath("//div[@class='user-streets']//li");
+    private final By btnConfirmRemovingLocator = By.xpath("//*[contains(@class, 'notification__actions')]//button");
+    private final By btnRemoveAddressInAddressCardLocator = By.xpath("//button[@aria-label='Delete']");
 
     public ProfilePage(WebDriver driver) {
         super(driver);
@@ -76,17 +78,11 @@ public class ProfilePage extends AbstractPage {
     }
 
     public ProfilePage addAddress(Address address) {
-        waitTime(1000);
-        btnAddAddress.click();
+        waitAndGet(btnAddAddressLocator).click();
+        waitAndGet(divChooseStreetLocator).click();
 
-        new WebDriverWait(driver, 10).until(d->driver.findElement(By.xpath("//div[div/text()='\u0423\u043b\u0438\u0446\u0430']"))).click();
-
-        WebElement searchInput = new WebDriverWait(driver, 10).
-                until(d->driver.findElement(By.xpath("//div[@class='search-street__modal-content']//input")));
-        searchInput.sendKeys(address.getStreet());
-        WebElement choiceBtn = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .until(d -> driver
-                        .findElement(By.xpath("//button[./div/text()=\"" + address.getStreet() + "\" and ./div/text()=\"" + address.getCity() + "\"]")));
+        waitAndGet(streetSearchInputLocator).sendKeys(address.getStreet());
+        WebElement choiceBtn = waitAndGet(By.xpath(String.format(choiceButtonStringLocator, address.getStreet(), address.getCity())));
         choiceBtn.click();
 
         inputHouseNumber.sendKeys(address.getHouseNumber());
@@ -96,36 +92,34 @@ public class ProfilePage extends AbstractPage {
 
         btnSubmitAddress.click();
 
-        waitTime(1000);
-        notification.findElement(By.className("notification__close-button")).click();
+        waitAndGet(waitAndGet(notificationLocator), btnCloseNotificationInNotificationLocator).click();
 
         logger.info("Address is added: " + address.toString());
         return this;
     }
 
     public boolean isAddressAdded() {
-        return notification.findElement(By.className("notification__content")).
+        return waitAndGet(notificationLocator).findElement(notificationContentInNotificationLocator).
                 getText().equals("\u0410\u0434\u0440\u0435\u0441 \u0434\u043e\u0431\u0430\u0432\u043b\u0435\u043d");
     }
 
     public int getAddressesCount() {
         waitTime(500);
-        return liAddressesDivs.size();
+        return waitAndGets(liAddressesDivsLocator).size();
     }
 
     public Address getLastAddedAddress() {
-        waitTime(2000);
-        return getAddressFromAddressLi(liAddressesDivs.get(0));
+        return getAddressFromAddressLi(waitAndGets(liAddressesDivsLocator).get(0));
     }
 
     public Address getAddressFromAddressLi(WebElement li) {
         WebElement divContent = li.findElement(By.className("custom-list__item-content"));
         String cityAndStreet = divContent.findElement(By.className("custom-list__item-content-primary")).getText();
         String houseNumberAndApartmentNumberAndEntranceAndFloor = divContent.findElement(By.className("custom-list__item-content-secondary")).getText();
-        List<String>cityAndStreetList = Arrays.asList(cityAndStreet.trim().split(", "));
-        List<String>houseNumberAndApartmentNumberAndEntranceAndFloorList = Arrays.asList(
+        List<String> cityAndStreetList = Arrays.asList(cityAndStreet.trim().split(", "));
+        List<String> houseNumberAndApartmentNumberAndEntranceAndFloorList = Arrays.asList(
                 houseNumberAndApartmentNumberAndEntranceAndFloor.replaceAll("[^0-9]+", " ")
-                .trim().split("\\s+"));
+                        .trim().split("\\s+"));
         String city = cityAndStreetList.get(0);
         String street = cityAndStreetList.get(1);
         String houseNumber = houseNumberAndApartmentNumberAndEntranceAndFloorList.get(0);
@@ -144,18 +138,15 @@ public class ProfilePage extends AbstractPage {
     }
 
     public ProfilePage removeAddress(int index) {
-        WebElement btnRemoveAddress = liAddressesDivs.get(index).findElement(By.xpath("//button[@aria-label='Delete']"));
+        WebElement btnRemoveAddress = waitAndGet(waitAndGets(liAddressesDivsLocator).get(index), btnRemoveAddressInAddressCardLocator);
         btnRemoveAddress.click();
 
-        WebElement btnConfirmRemoving = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).
-                until(d->driver.
-                        findElement(By.className("notification__actions")).
-                        findElement(By.tagName("button")));
+        WebElement btnConfirmRemoving = waitAndGet(btnConfirmRemovingLocator);
         btnConfirmRemoving.click();
         return this;
     }
 
     public ProfilePage removeLastAddress() {
-        return removeAddress(getAddressesCount()-1);
+        return removeAddress(getAddressesCount() - 1);
     }
 }
